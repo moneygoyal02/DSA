4148aa9e9edd6a78d0e6e6e3dfcf1f9ce3dec06d
moneygoyal02 <goyalmoney95@gmail.com>
2024-05-29 12:38:57 +0530

    first done

--- PRACTICE/1.cpp
+++ PRACTICE/1.cpp
@@ -1,8 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
+// max element of array 
 
 using namespace std;
 
 int main() {
-    cout << "Hello, World!";
-    return 0;
+    int getmax(int arr[], int n);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << getmax(arr, n);
+
+}
+
+int getmax(int arr[] , int n){
+    int max = arr[0];
+
+    for(int i = 1; i < n; i++){
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    };
+
+    return max;
 }
